48d022f957e0dda7e57f200f10584b87b724c16f
Felipe Osorio <felipe.g.osorio@gmail.com>
2020-08-13 15:15:13 -0300

    Refatorando - removendo dependências não utilizadas

--- CreditCard/src/main/java/br/com/itau/creditCard/dto/UpdateCreditCardRequest.java
+++ CreditCard/src/main/java/br/com/itau/creditCard/dto/UpdateCreditCardRequest.java
@@ -2,9 +2,6 @@ package br.com.itau.creditCard.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
 public class UpdateCreditCardRequest {
 
     @JsonProperty("ativo")
